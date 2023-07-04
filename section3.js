a6538ead154a6227a033f43774b3bc1b8e0c37f0
hwakachan <hi_wakachan0823@icloud.com>
2023-07-04 23:54:41 +0900

    Function Declaratioins vs Expressions

--- section3.js
+++ section3.js
@@ -32,10 +32,19 @@ const appleOrangeJuuice = fruitProcessor(2, 4);
 console.log(appleOrangeJuuice);
 */
 
+// Function declaration
 function calcAge1(birthYear) {
   return 2037 - birthYear;
 
 }
 
 const age1 = calcAge1(1997);
-console.log(age1);
+
+// Function expression
+const calcAge2 = function (birthYear) {
+  return 2037 - birthYear;
+}
+
+const age2 = calcAge2(1991);
+
+console.log(age1, age2);
