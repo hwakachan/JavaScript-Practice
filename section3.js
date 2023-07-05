9fa10aca150d1599a6f167107128b3a7877309f6
hwakachan <hi_wakachan0823@icloud.com>
2023-07-05 23:49:27 +0900

    use arrow function

--- section3.js
+++ section3.js
@@ -48,3 +48,16 @@ const calcAge2 = function (birthYear) {
 const age2 = calcAge2(1991);
 
 console.log(age1, age2);
+
+//Arrow function
+const calcAge3 = birthYear => 2037 - birthYear;
+const age3 = calcAge3(1991);
+console.log(age3);
+
+const yearsUntilRetirement = birthYear => {
+  const age = 2037 - birthYear;
+  const retirement = 65 - age;
+  return retirement;
+}
+
+console.log(yearsUntilRetirement(1991));
