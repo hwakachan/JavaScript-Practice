ab5f33f482416630bf967ef85396eb82b6dd0b1c
hwakachan <hi_wakachan0823@icloud.com>
2023-07-08 20:27:36 +0900

    practice use function

--- section3.js
+++ section3.js
@@ -32,6 +32,7 @@ const appleOrangeJuuice = fruitProcessor(2, 4);
 console.log(appleOrangeJuuice);
 */
 
+/*
 // Function declaration
 function calcAge1(birthYear) {
   return 2037 - birthYear;
@@ -58,6 +59,21 @@ const yearsUntilRetirement = birthYear => {
   const age = 2037 - birthYear;
   const retirement = 65 - age;
   return retirement;
+  
 }
 
 console.log(yearsUntilRetirement(1991));
+*/
+function cutFruitPieces(fruit){
+  return fruit * 4;
+}
+
+function fruitProcessor(apples, oranges) {
+  const applePieces = cutFruitPieces(apples);
+  const orangePieces = cutFruitPieces(oranges);
+
+  const juice = `Juice with ${applePieces} apples and ${orangePieces} oranges.`;
+  return juice;
+}
+
+console.log(fruitProcessor(2, 3));
